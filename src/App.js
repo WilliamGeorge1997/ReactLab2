a2f7b1b5caabac0e012fe79dca098c9b3484578f
William George <williamgeorge1997x@gmail.com>
2024-03-03 22:38:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Tasks from "./Components/Tasks/Tasks";
+import StandardErrorBoundry from "./Components/LCM/StandardErrorBoundry";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StandardErrorBoundry>
+        <Tasks />
+      </StandardErrorBoundry>
+    </>
   );
 }
 
